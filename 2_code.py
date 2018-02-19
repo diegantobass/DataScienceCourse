a806e0ac7526046a7eff4c72b267d4c0fae0bf98
diegantobass <diego.antolinosbasso@sciencespo.fr>
2018-02-19 12:36:33 +0100

    update second lesson with content, corresponding exercices, and code

--- 2_code.py
+++ 2_code.py
@@ -1,68 +1,53 @@
 # -*- coding: utf8 -*-
 
-# Ceci est un commentaire
-# Ce script reprend le travail effectué durant la séance 2
-
-print("--- Python 101 - Séance 2 ---")
-print("-----------------------------")
-
+# Ceci est un commentaire, il est précédé d'un dièse
+# Ce script reprend le travail effectué durant la séance 1 et 2 du cours de Python : Data Science for Dummies
+print("--- Python - Data Science for Dummies ---")
+print("-----------------------------------------")
 """
-Ceci est un autre commentaire
-Il est crucial de commenter son code de façon claire et précise ! Documenter le code fait partie du code
-Les commentaires entre triples guillemets et après les dièses ne sont pas executé lorsque vous lancez le script
+Il est crucial de commenter de façon claire et précise ! Documenter le code fait partie du code
+Les commentaires entre triples guillemets permettent de commenter sur plusieurs lignes.
 """
 
-print("---- afficher un message ----")
+###
 
+print("--- afficher un message ---")
 print("Let's go") # les guillemets doubles permettent d'inclure des apostrophes dans le message
-print('"Let us go"') # les simples permettent d'inclure des doubles
-print('Let\'s go') # il est également possible "d'échapper" les caractères spéciaux pour que python ne les considère pas comme du code
+print('Let "us" go') # les simples permettent d'inclure des doubles
+print('Let\'s go') # il est également possible "d'échapper" les caractères spéciaux pour qu'ils ne soient pas considérér comme du code
 # print('Let's go') # cette ligne de code ne fonctionnera pas (dé-commentez la et essayez de lancer le script)
 
-print("--- déclarer des variables --")
+###
 
-# stocker une valeur dans une variable
-variable = "Hello, World!"
+print("--- déclarer des variables ---")
+variable = "Hello, World!" # stocker une valeur dans une variable
 print(variable)
 
-# les types de valeur
-integer = 725 # un entier
-string = "Hello, World!" # un texte
-float_ = 1.23 # un nombre décimal
-booleen = True # un booléen : soit "vrai", soit "faux"
+###
 
-print(integer) # j'affiche la valeur stockée dans la variable "integer"
-print(type(integer)) # j'affiche le type de la variable integer
+print("--- les types de variables ---")
+integer = 725 # un nombre entier, integer
+string = "Hello, World!" # une chaîne de caractère, string
+floatnumber = 1.23 # un nombre décimal, float
+booleen = True # un booléen : soit "vrai", soit "faux", boolean
+rien = None # l'absence de valeur, none
+
+print(floatnumber) # j'affiche la valeur stockée dans la variable "floatnumber"
+print(type(floatnumber)) # j'affiche le type de la variable floatnumber
 
 """
 print() et type() sont des fonctions du langage python. Elles prennent un argument entre leurs parenthèses
-print() affiche sont argument dans le terminal. type() nous donne le type de sont argument
-print(type(variable)) va donc afficher dans le terminal le résultat de la fonction type(variable)
+print() affiche sont argument dans le terminal. type() renvoie le type de sont argument
+print(type(variable)) va donc afficher dans le terminal ce que renvoie (le resultat de) la fonction type(variable)
 les deux lignes suivantes font la même chose en deux étapes plutôt qu'une seule
 """
 
 type_integer = type(integer)
 print(type_integer)
 
-# formater un message à afficher dans le terminal
-print("J'ai affiché {} fois {} dans ma vie, c'est {}...".format(integer, string, booleen))
-# dans chacunes des paires d'accolades est injecté l'une des variables de la fonction format() dans l'ordre
+###
 
-print("----- input utilisateur -----")
-
-# input utilisateur
-user_input = input("Combien font 2 et 2 ? ")
-print("2 et 2 font {}.".format(user_input))
-
-string_input = input("Comment t'appelles-tu ? ") # fonction input() pour python 3
-# string_input = raw_input("Comment t'appelles-tu ? ") # fonction raw_input() pour python 2 car input() ne prend pas de texte dans python 2.7
-print("Tu t'appelles {}.".format(string_input))
-
-print('{} a affiché {} fois "{}" dans son terminal, c\'est un data-scientist.'.format(string_input, integer, variable)) # notez l'utilisation des guillemets
-
-print("-- opérateurs mathématiques -- bonus")
-
-# opérateurs mathématiques
+print("--- les opérateurs mathématiques ---")
 addition = 7 + 23
 subtraction = 108 - 18
 multiplication = 1000 * 0.5
@@ -75,15 +60,19 @@ print(more)
 more = more + 4 - addition # on modifie la variable : elle est maintenant égal à son ancienne valeur plus 4 moins la valeur de addition
 print(more)
 
-print("--- les conditions avec if --- bonus")
+###
 
+print("--- les conditions avec if ---")
 booleen = True # on déclare un booleen avec la valeur "vrai"
 
 if booleen == True: # si la variable booleen est égale à "vrai"
 	print("sweet success") # alors j'exécute ce code
 
-if booleen == False:
-	print("epic fail") # ce code ne sera pas exécuté puisque booleen == "True"
+if subtraction == 18: # si la variable booleen est égale à "faux"
+	print("epic fail") # alors j'execute ce bloc de code
+
+else: # sinon j'execute le code suivant
+	print("whatever")
 
 """
 notez les deux points qui suivent la condition formulée après le 'if' -> if condition: retour à la ligne
@@ -91,9 +80,30 @@ notez l'indentation : la tabulation en début de ligne avant les instructions qu
 ces éléments appartiennent à la syntaxe obligatoire de python, ils sont nécessaires sinon "Syntax Error"
 """
 
-un_entier = 4 # on déclare une variable avec la valeur 4
+###
+### Nous avons été jusque là à la séance 1
+###
 
+print("--- toujours plus de conditions avec if ---")
+un_entier = 4 # on déclare une variable avec la valeur 4
 if un_entier == 4:
 	print("Ce code s'exécute car la variable un_entier est égale à 4")
 
-print("-----------------------------")
+###
+
+print("--- formater un message à afficher ---")
+print("J'ai affiché {} fois {} dans ma vie, c'est {}...".format(integer, string, booleen))
+# dans chacunes des paires d'accolades est injecté l'une des variables de la fonction format() dans l'ordre
+
+###
+
+print("----- input utilisateur -----")
+user_input = input("Combien font 2 et 2 ? ") # on demande un input à l'utilisateur qui doit y répondre pour que le code continue
+print("2 et 2 font {}.".format(user_input))
+
+string_input = input("Comment t'appelles-tu ? ") # on pose une question à l'utilisateur dont on stocke le résultat dans une variable
+print("Tu t'appelles {}.".format(string_input))
+
+print('{} a affiché {} fois "{}" dans son terminal, c\'est un data-scientist.'.format(string_input, integer, variable)) # notez l'utilisation des guillemets
+
+###
--- 2_exo.md
+++ 2_exo.md
@@ -1,40 +1,42 @@
 # Ne faites qu'un des 3 exercices proposés
 
 ### 1. L'addition, s'il vous plait !
-
 L'énoncé suivant décrit une manière de calculer le prix d'un repas, TVA et pourboire compris. Le but de cet exercice est de réussir à coder ce que décrit l'énoncé avec des mots.
 
 ~~~
-Déclarer une variable dont la valeur est un input utilisateur. Ce sera le prix de notre repas au restaurant.
+Déclarer une variable dont la valeur est un input utilisateur.Ce sera le prix de notre repas au restaurant.
 Déclarer une deuxième variable contenant le taux de TVA sur la restauration.
-Déclarer une troisième variable contenant le pourcentage de pourboire que vous souhaitez (devriez) laisser.
+Déclarer une troisième variable contenant le pourcentage de pourboire que vous souhaitez ou devriez laisser.
 
 Mettez à jour le prix du repas en y ajoutant la TVA
-Déclarer une variable "total" dont la valeur est le prix TTC auquel vous ajoutez votre pourboire.
+Déclarer une variable dont la valeur est le prix TTC auquel vous ajoutez votre pourboire.
 
 Afficher un texte dans le terminal qui annonce le prix du repas
 ~~~
 
 Le rendu sera un script additionsvp_DAB.py (DAB c'est mes initiales, mettez les votre, svp) à m'envoyer par mail. Je réponds aux questions par email.
 
-### 2. Répondez aux questions suivantes
+### 2. Pierre papier ciseaux
+Programmez un jeu de chifoumi.
 
-Les questions suivantes reprennent quelques notions du code vu en cours.
-
-1. Affichez le message "Hello, World!" grâce à la fonction print()
-2. Déclarer une variable dont la valeur est 42. Déclarer une seconde variable dont la valeur est le _type_ de la première (fonction type()). Affichez les deux variables.
-3. Stocker deux nombres dans deux variables distinctes. Effectuer une opération mathématique avec ces deux variables (voir les opérateurs mathématiques dans [le code du cours n°2](2_code.py)). Affichez le résultat.
-4. Posez une question à l'utilisateur dans le terminal grâce à la fonction input(). Affichez le résultat.
-
-Le rendu sera un script questions_DAB.py (DAB c'est mes initiales, mettez les votre, svp) à m'envoyer par mail. Je réponds aux questions par email.
+Importer le package random au début de votre code de la façon suivante
+~~~python
+import random
+~~~
+Déclarer une variable input utilisateur. Elle doit contenir "papier", "pierre" ou "ciseaux".
+Déclarer une seconde variable qui contient le résultat de la fonction suivante
+~~~python
+random.choice(["papier", "pierre", "ciseaux"])
+~~~
+À l'aide de bloc conditionnels, afficher ensuite le résultat de la "confrontation" entre l'utilisateur et la machine représentée par la fonction random.
 
 ### 3. Freestyle
-
 Envoyez-moi d'ici à lundi n'importe quel bout de code python que vous aurez écrit. Peu importe ce qu'il fait.
 
 __Deux conditions à remplir__ : 
-1. le code doit être commenté.
-2. les commentaires doivent contenir des questions sur les points que vous ne pensez pas avoir compris, ou ce qui vous a manqué.
+1. le code doit être commenté, je ne m'amuserai pas à deviner ce qu'il est censé faire.
+2. les commentaires doivent contenir des questions sur les points que vous ne pensez pas avoir compris, ce qui vous a manqué, ce que vous aimeriez rajouter.
 5. Si le code s'exécute sans erreur c'est mieux quand même.
 
-Vous pouvez faire l'un des exercices de [la liste disponible ici](http://www.practicepython.org/) si vous manquez d'inspiration.
+### 4. Exercices en ligne
+Vous pouvez faire l'un des exercices de [la liste disponible ici](http://www.practicepython.org/).
